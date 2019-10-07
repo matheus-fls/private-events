87cb039b29639d76edd36f09794e3d035c986def
matheus-fls <matheus_fls@yahoo.com.br>
2019-10-07 19:33:17 -0300

    Create basic header partial and Events controller, update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   get    '/signup', to: 'users#new'
   post   '/signup', to: 'users#create'
-  #get    '/user'  , to: 'users#show'
 
   get    '/login',  to: 'sessions#new'
   post   '/login',  to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
+  get    '/events', to: 'events#new'
+  post   '/events', to: 'events#create'
+
+  resources :events,   only: [:new, :create, :show, :index]
   resources :sessions, only: [:new, :create, :destroy]
   resources :users,    only: [:new, :create, :show]
 end

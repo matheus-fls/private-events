d9f9b3fe81ed75c06fa9ef7b042fdcdf500ab5a1
matheus-fls <matheus_fls@yahoo.com.br>
2019-10-07 13:32:16 -0300

    Create signup view, update create user, add validations to user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,22 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(params_user)
+    if @user.valid?
+      @user.save 
+      redirect_to show_url
+    else
+      flash[:info] = 'Invalid username/email'
+      render 'new'
+    end
   end
 
   def show
   end
+
+  private 
+
+    def params_user
+      params.require(:user).permit(:name, :email)
+    end
 end

49d0a4a4d6c349c1dbf673b80eec9950671b22e2
AnthonyTC89 <ptonyptc@gmail.com>
2019-10-08 15:10:03 -0500

    model attendance created, model user, event changed

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
+  has_many :attendances
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
+  has_many :attendees, class_name: 'User', through: :attendances
 end

fbc7fbcbcb09ad3cac163bf04369512b8ef20716
AnthonyTC89 <ptonyptc@gmail.com>
2019-10-10 17:20:34 -0500

    user_controller rspec some tests

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,10 +1,27 @@
+require 'rails_helper'
 require "./app/controllers/users_controller"
 
 RSpec.describe UsersController do 
   describe "Create new user" do 
-    it "Create a blank user" do 
-      app = ApplicationController.new
-      expect(true).to true
+    it "Create a nil user" do 
+      @user = User.new()
+      expect(@user).to_not be_valid
     end
+
+    it "Create a blank user" do 
+      @user = User.new(name:'', email:'')
+      expect(@user).to_not be_valid
+    end
+
+    it "Create a valid user" do 
+      @user = User.new(name:'Anthony', email:'antman@gmail.com')
+      expect(@user).to be_valid
+    end
+
+    it "Create a user with invalid email" do 
+      @user = User.new(name:'Foobar', email:'foobar@gmail')
+      expect(@user).to_not be_valid
+    end
+
   end
 end

94e3b95d9b02ee46334d9772f13e1971af52c853
matheus-fls <matheus_fls@yahoo.com.br>
2019-10-11 17:06:45 -0300

    Fix a bug that prevented the creation of an event on the same day

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
 class Event < ApplicationRecord
-  has_many :attendances
-  belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
-  has_many :attendees, class_name: 'User', through: :attendances
+  has_many   :attendances
+  belongs_to :creator,   class_name: 'User', foreign_key: 'user_id'
+  has_many   :attendees, class_name: 'User', through: :attendances
 
-  validates :name, presence: true
+  validates :name,        presence: true
   validates :description, presence: true
-  validates :date, presence: true
-  validate :future_event
+  validates :date,        presence: true
+  validate  :future_event
 
   private
 
   def future_event
-    errors.add(:date, "Can't be in the past!") if date.past?
+    errors.add(:date, "Can't be in the past!") if date < Date.today
   end
 end

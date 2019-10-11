ba1848de76de717c9f5dbd16cf73130e6e82d93e
stickler-ci <support@stickler-ci.com>
2019-10-11 20:53:40 +0000

    Fixing style errors.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,19 +1,23 @@
 # frozen_string_literal: true
 
 class Event < ApplicationRecord
-  has_many   :attendances
-  belongs_to :creator,   class_name: 'User', foreign_key: 'user_id'
-  has_many   :attendees, class_name: 'User', through: :attendances
+  has_many :attendances
+  belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
+  has_many :attendees, class_name: 'User', through: :attendances
 
-  validates :name,        presence: true
+  validates :name, presence: true
   validates :description, presence: true
-  validates :date,        presence: true
-  validate  :future_event
+  validates :date, presence: true
+  validate :future_event
 
-  scope :upcoming_events, -> (user_id) { where(["date >= ? and user_id = ?", 
-                                                 Date.today, user_id]) }
-  scope :past_events    , -> (user_id) { where("date <  ?", Date.today) &&
-                                         where("user_id = ?", user_id) }                                 
+  scope :upcoming_events, lambda { |user_id|
+                            where(['date >= ? and user_id = ?',
+                                   Date.today, user_id])
+                          }
+  scope :past_events, lambda { |user_id|
+                        where('date <  ?', Date.today) &&
+                          where('user_id = ?', user_id)
+                      }
 
   private
 

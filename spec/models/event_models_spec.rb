6903eb56acd86541e7777519fd0c764c42167fb0
AnthonyTC89 <ptonyptc@gmail.com>
2019-10-11 12:36:41 -0500

    event models spec done

--- spec/models/event_models_spec.rb
+++ spec/models/event_models_spec.rb
@@ -3,35 +3,39 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  subject { Event.new }
+  
+  let(:user) {User.create!(name:'Foobar', email:'foobar@bar.com')}
+
+  subject { Event.create!(name: 'Anything',description: 'Anything', 
+  date: Time.now + 1.month, user_id: user.id) }
 
   it 'is valid with valid attributes' do
-    subject.name = 'Anything'
-    subject.description = 'Anything'
-    subject.date = DateTime.now + 1.month
-    subject.user_id = 1
     expect(subject).to be_valid
   end
 
-  it 'is not valid without a title' do
-    expect(subject).to_not be_valid
+  it "a user can have more than one event" do
+
+    event1 = Event.create!(name: 'Anything1',description: 'Anything1', 
+      date: Time.now + 1.month, user_id: user.id)
+    event2 = Event.create!(name: 'Anything2',description: 'Anything2', 
+      date: Time.now + 2.month, user_id: user.id)
+
+    expect(user.reload.events).to eq([event1, event2])
   end
 
   it 'is not valid without a description' do
-    subject.name = 'Anything'
+    subject.description = ''
     expect(subject).to_not be_valid
   end
 
-  it 'is not valid without a start_date' do
-    subject.name = 'Anything'
-    subject.description = 'Lorem ipsum dolor sit amet'
+  it 'is not valid without a date' do
+    subject.date = nil
+    expect {subject.valid?}.to raise_error(NoMethodError)
+  end
+
+  it 'is not valid without an user id' do
+    subject.user_id = nil
     expect(subject).to_not be_valid
   end
 
-  it 'is not valid without a end_date' do
-    subject.name = 'Anything'
-    subject.description = 'Lorem ipsum dolor sit amet'
-    subject.date = DateTime.now
-    expect(subject).to_not be_valid
-  end
 end

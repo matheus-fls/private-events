94b4d9d4a5294566a57c396a0263409020582c41
stickler-ci <support@stickler-ci.com>
2019-10-11 22:00:12 +0000

    Fixing style errors.

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -5,9 +5,12 @@ class Attendance < ApplicationRecord
   belongs_to :event
   validates_uniqueness_of :user_id, scope: :event_id
 
-  scope :upcoming_events, -> (user_id) { joins(:event).where('attendances.user_id = ? 
-                                          and events.date >= ?', user_id, Date.today) }
-  scope :past_events, -> (user_id) { joins(:event).where('attendances.user_id = ? 
-                                          and events.date < ?', user_id, Date.today) }
-
+  scope :upcoming_events, lambda { |user_id|
+                            joins(:event).where('attendances.user_id = ?
+                                          and events.date >= ?', user_id, Date.today)
+                          }
+  scope :past_events, lambda { |user_id|
+                        joins(:event).where('attendances.user_id = ?
+                                          and events.date < ?', user_id, Date.today)
+                      }
 end

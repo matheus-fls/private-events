228a2f677fd98fcc968f12b365e5e92e70b78766
stickler-ci <support@stickler-ci.com>
2019-10-11 17:05:10 +0000

    Fixing style errors.

--- spec/models/event_models_spec.rb
+++ spec/models/event_models_spec.rb
@@ -1,34 +1,36 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe Event, :type => :model do
+RSpec.describe Event, type: :model do
   subject { Event.new }
 
-  it "is valid with valid attributes" do
-    subject.name = "Anything"
-    subject.description = "Anything"
+  it 'is valid with valid attributes' do
+    subject.name = 'Anything'
+    subject.description = 'Anything'
     subject.date = DateTime.now + 1.month
     subject.user_id = 1
     expect(subject).to be_valid
   end
 
-  it "is not valid without a title" do
+  it 'is not valid without a title' do
     expect(subject).to_not be_valid
   end
 
-  it "is not valid without a description" do
-    subject.name = "Anything"
+  it 'is not valid without a description' do
+    subject.name = 'Anything'
     expect(subject).to_not be_valid
   end
 
-  it "is not valid without a start_date" do
-    subject.name = "Anything"
-    subject.description = "Lorem ipsum dolor sit amet"
+  it 'is not valid without a start_date' do
+    subject.name = 'Anything'
+    subject.description = 'Lorem ipsum dolor sit amet'
     expect(subject).to_not be_valid
   end
 
-  it "is not valid without a end_date" do
-    subject.name = "Anything"
-    subject.description = "Lorem ipsum dolor sit amet"
+  it 'is not valid without a end_date' do
+    subject.name = 'Anything'
+    subject.description = 'Lorem ipsum dolor sit amet'
     subject.date = DateTime.now
     expect(subject).to_not be_valid
   end
--- spec/models/user_models_spec.rb
+++ spec/models/user_models_spec.rb
@@ -1,22 +1,24 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
+RSpec.describe User, type: :model do
   subject { User.new }
 
-  it "is valid with valid attributes" do
-    subject.name = "Foo Bar"
-    subject.email = "foobar@foo.bar"
+  it 'is valid with valid attributes' do
+    subject.name = 'Foo Bar'
+    subject.email = 'foobar@foo.bar'
     expect(subject).to be_valid
   end
 
-  it "is not valid without a name" do
-    subject.name = ""
+  it 'is not valid without a name' do
+    subject.name = ''
     expect(subject).to_not be_valid
   end
 
-  it "is not valid without an email" do
-    subject.name = "Foo Bar"
-    subject.email = ""
+  it 'is not valid without an email' do
+    subject.name = 'Foo Bar'
+    subject.email = ''
     expect(subject).to_not be_valid
   end
 end
